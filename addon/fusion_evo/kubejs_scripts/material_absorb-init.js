f9dea34abb89c790571a0ce16e88cb3c82606cff
cj-salad0 <cjreed13007@gmail.com>
2026-02-22 19:12:27 -0500

    global object test absorb materials

---
+++ addon/fusion_evo/kubejs_scripts/material_absorb-init.js
@@ -0,0 +1,11 @@
+global.os_materials ={
+
+    name: "material_absorb_stats",
+
+    materials: 
+    [
+        {name: "iron",  smallAbsorbList: ["minecraft:iron_ingot", "minecraft:lantern"]}
+    ]
+
+
+}
